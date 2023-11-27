00aa2d96e2f9dab7f29c476a89cc00fb6cbe8064
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-11-27 22:55:35 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.Services;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Blog blog = new Blog()
+            {
+                Title = "testtitle",
+                Description = "testdes",
+                UserId = 1
+            };
+            //BlogService.Create(blog);
+            //foreach (Blog item in BlogService.GetAll())
+            //{
+            //    Console.WriteLine($"{item.Id} {item.Title} {item.Description} {item.UserId}");
+            //}
+            User user = new User()
+            {
+                Name = "Test",
+                Surname = "Test",
+                Password = "Test123",
+            };
+            UserService.Register(user);
+        }
+    }
+}
